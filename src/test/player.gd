02a9f4166a22b114d67364339d8d89997271a60c
DHDC-21 <douglas.dionysio@outlook.com.br>
2026-09-03 12:17:37 -0300

    change main menu and add more questions for quiz

--- src/test/player.gd
+++ src/test/player.gd
@@ -1,34 +1,29 @@
 extends CharacterBody3D
 
-
 @export var speed: float = 5.0 
 @export var jump_velocity: float = 4.5
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta: float) -> void:
-
+	# Aplica a gravidade
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
+	# Aplica o pulo
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 
-	var direction = Vector3.ZERO
+	# Captura o vetor de entrada em 8 direções (Esquerda, Direita, Frente, Trás)
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
-	if Input.is_action_just_pressed("move_forward"):
-		direction.z -= 1
-	if Input.is_action_just_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_just_pressed("move_back"):
-		direction.x += 1
-	if Input.is_action_just_pressed("move_right"):
-		direction.z += 1
-
+	# Aplica a velocidade de movimento nos eixos X e Z
 	if direction != Vector3.ZERO:
-		direction = direction.normalized()
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
 	move_and_slide()
--- src/ui/menu/main.gd
+++ src/ui/menu/main.gd
@@ -1,11 +1,29 @@
 extends CanvasLayer
 
 
+@onready var popup_sair = $Control/VBoxContainer/VBoxContainer/BtnCreditos/PopupSair
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+
+func _on_btn_iniciar_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_creditos_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_sair_pressed() -> void:
+	pass # Replace with function body.
+	popup_sair.popup_centered()
+
+func _on_popup_sair_confirmed() -> void:
+	get_tree().quit()
