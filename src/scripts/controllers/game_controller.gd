1d74fdc7cd41a18ca3427de4b19be38e188ea55d
DHDC-21 <douglas.dionysio@outlook.com.br>
2026-09-09 11:41:32 -0300

    change game_controller

--- src/scripts/controllers/game_controller.gd
+++ src/scripts/controllers/game_controller.gd
@@ -20,7 +20,7 @@ func _ready() -> void:
 				buttons.append(child)
 				#print("Botão: ", child.name)
 	load_quiz()
-	
+
 	print("Total de botões encontrados: ", buttons.size())
 
 func load_quiz() -> void:
@@ -31,3 +31,10 @@ func load_quiz() -> void:
 
 	for i in range(min(buttons.size(), question.question_choices.size())):
 		buttons[i].text = question.question_choices[i]
+		buttons[i].pressed.connect(_buttons_answer.bind(buttons[i]))
+
+func _buttons_answer(button: Button) -> void:
+	if quiz.theme[index].correct_answer == button.text:
+		button.modulate = color_right
+	else:
+		button.modulate = color_wrong
